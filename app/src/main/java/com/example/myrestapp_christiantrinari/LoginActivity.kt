33ffd516cb8ba16b612168d8baab42789d34fbb9
binf01-Trinari <trinarichristian@gmail.com>
2025-06-04 10:40:46 +0200

    Creato intent a menu activity

--- app/src/main/java/com/example/myrestapp_christiantrinari/LoginActivity.kt
+++ app/src/main/java/com/example/myrestapp_christiantrinari/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myrestapp_christiantrinari
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,15 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val accedi = findViewById<Button>(R.id.accedi)
+        accedi.setOnClickListener {
+            menuIntent()
         }
+    }
+
+    private fun menuIntent(){
+        val mioIntent = Intent(this, MenuActivity::class.java)
+        startActivity(mioIntent)
     }
 }
