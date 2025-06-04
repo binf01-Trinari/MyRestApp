ba654bad33efb6b757acd207ebc416af6b4a49ed
binf01-Trinari <trinarichristian@gmail.com>
2025-06-04 10:46:53 +0200

    Creato intent per la web view di netflix

--- app/src/main/java/com/example/myrestapp_christiantrinari/MenuActivity.kt
+++ app/src/main/java/com/example/myrestapp_christiantrinari/MenuActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myrestapp_christiantrinari
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,14 @@ class MenuActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
 
+        val netflix = findViewById<ImageButton>(R.id.netflix)
+        netflix.setOnClickListener {
+            netflixIntent()
+        }
+    }
+
+    private fun netflixIntent(){
+        val mioIntent = Intent(this, WebViewActivity::class.java)
+        startActivity(mioIntent)
     }
 }
